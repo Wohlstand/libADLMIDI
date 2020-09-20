9b51dd7d667e97338a92711543fc3e942faae52f
Vitaly Novichkov <Wohlstand@users.noreply.github.com>
2020-09-21 00:43:58 +0300

    Added WinMM driver for windows (#236)
    * ADLMIDI2: Fixed an MSVC build
    However, it may work glitchy!
    
    * Experimental WinMM MIDI driver
    
    TODO: Make a control panel to configure the driver
    
    * WinMM-DRV: A workaround for older MinGW
    
    * WinMM-DRV: WIP control panel appled / tool
    
    * WinMM-DRV: Better test a driver's work
    
    * WinMM-DRV: More progress on a control panel
    
    * WinMM-DRV: And more progress on this control panel
    It's almost completed, I need to make the working setup through a registry.
    
    * WinMM-DRV: Driver is almost ready
    However, control panel can't ping a driver to reload settings yet
    
    * WinMM-DRV: Fix the missing volume model setup
    
    * WinMM-DRV: Setup reload on the fly now work!
    
    * WinMM-DRV: Stabilize the thing
    
    * WinMM-DRV: Stabilizing
    
    * WinMM-DRV: Avoid all "setup" in names
    to avoid a stupid PCA thing:
    https://stackoverflow.com/questions/17660404/how-to-programmatically-disable-program-compatibility-assistant-in-windows-7-and
    
    * A small warning fix at the WOPL module
    
    * WinMM-DRV: Fixed some warnings
    
    * WinMM-DRV: Avoid PCA dialog on the installer
    
    * WiNMM-DRV: Make the linking of pthread being optional
    Required to use MinGW-w64 toolchain with the "win32" threading mode
    
    * WinMM-DRV: Receive the MODM_RESET to reset the MIDI state
    
    * WinMM-DRV: Attempt to fix a sound distorsion on song song
    
    * WinMM-Drv: Fixed a build on some older MinGW toolchains
    
    * WinMM-Drv: Fixed default settings not being loaded

---
+++ utils/winmm_drv/cpl/resource.h
@@ -0,0 +1,50 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by adlsetup.rc
+//
+#include <winresrc.h>
+
+#define MANIFSEST_RES                   1
+#define MANIFSEST_RES_CPL               123
+#define IDD_CONFIG_BOX                  101
+#define IDI_ICON1                       102
+#define IDC_DRIVERNAME                  103
+#define IDC_DRIVERDESC                  104
+
+#define IDC_APPLYBUTTON                 1000
+#define IDC_VOLUMEMODEL                 1001
+#define IDC_VM_LABEL                    1002
+#define IDC_BANK_ID                     1003
+#define IDC_INS_BANK                    1004
+#define IDC_BANK_INTERNAL               1005
+#define IDC_BANK_EXTERNAL               1006
+#define IDC_BANK_PATH                   1007
+#define IDC_BROWSE_BANK                 1008
+#define IDC_RESTORE_DEFAULTS            1010
+#define IDC_FLAG_TREMOLO                1011
+#define IDC_FLAG_VIBRATO                1012
+#define IDC_FLAG_SOFTPAN                1013
+#define IDC_EMULATOR                    1014
+#define IDC_NUM_CHIPS                   1018
+#define IDC_NUM_4OPVO                   1019
+#define IDC_FLAG_SCALE                  1020
+#define IDC_FLAG_FULLBRIGHT             1021
+#define IDC_ABOUT                       1022
+#define IDC_CHIPEMU_LABEL               1023
+#define IDC_CHIPNUM_LABEL               1024
+#define IDC_4OPSNUM_LABEL               1025
+#define IDC_RESET_SYNTH                 1026
+#define IDC_BOTTOMNOTE_LABEL            1027
+
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1028
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
