51134d107d7b1771bc7dbae1e67cb8b22d330962
freq-mod <32672779+freq-mod@users.noreply.github.com>
2021-11-30 12:51:47 +0100

    oops
    ADL != OPN after all

--- include/adlmidi.h
+++ include/adlmidi.h
@@ -658,8 +658,8 @@ enum ADL_Emulator
     ADLMIDI_EMU_OPAL,
     /*! Java */
     ADLMIDI_EMU_JAVA,
-   /*! VGM file dumper (required for MIDI2VGM) */
-    OPNMIDI_VGM_DUMPER,
+    /*! VGM file dumper (required for MIDI2VGM) */
+    ADLMIDI_VGM_DUMPER,
     /*! Count instrument on the level */
     ADLMIDI_EMU_end
 };
