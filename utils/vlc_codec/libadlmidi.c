37b08242a35f403da6e916fb9b1051d29cb9b552
Wohlstand <admin@wohlnet.ru>
2021-12-15 02:11:23 +0300

    VLC Plugin: Added the auto-arpeggio settings field

--- utils/vlc_codec/libadlmidi.c
+++ utils/vlc_codec/libadlmidi.c
@@ -81,6 +81,10 @@
 #define FULL_RANGE_CC74_LONGTEXT N_( \
     "Scale range of CC-74 \"Brightness\" with full 0~127 range. By default is only 0~64 affects the sounding.")
 
+#define ENABLE_AUTO_ARPEGGIO_TEXT N_("Enable auto-arpeggio")
+#define ENABLE_AUTO_ARPEGGIO_LONGTEXT N_( \
+    "Enables an automatical arpeggio to keep chords playing when there is no enough free physical voices of chips.")
+
 static const int volume_models_values[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 static const char * const volume_models_descriptions[] =
 {
@@ -165,6 +169,9 @@ vlc_module_begin ()
     add_bool( CONFIG_PREFIX "full-range-brightness", false, FULL_RANGE_CC74_TEXT,
               FULL_RANGE_CC74_LONGTEXT, true )
 
+    add_bool( CONFIG_PREFIX "enable-auto-arpeggio", true, ENABLE_AUTO_ARPEGGIO_TEXT,
+              ENABLE_AUTO_ARPEGGIO_LONGTEXT, true )
+
     add_bool( CONFIG_PREFIX "full-panning", true, FULL_PANNING_TEXT,
               FULL_PANNING_LONGTEXT, true )
 
@@ -219,6 +226,8 @@ static int Open (vlc_object_t *p_this)
     adl_setSoftPanEnabled(p_sys->synth, var_InheritBool(p_this, CONFIG_PREFIX "full-panning"));
 
     adl_setFullRangeBrightness(p_sys->synth, var_InheritBool(p_this, CONFIG_PREFIX "full-range-brightness"));
+
+    adl_setAutoArpeggio(p_sys->synth, var_InheritBool(p_this, CONFIG_PREFIX "enable-auto-arpeggio"));
 
     char *bank_path = var_InheritString (p_this, CONFIG_PREFIX "custombank");
     if (bank_path != NULL)
