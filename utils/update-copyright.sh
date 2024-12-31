07653c171f61f38113e61804171d4e2260615baa
Wohlstand <admin@wohlnet.ru>
2025-01-01 01:47:54 +0300

    Small fix of the copyright update script

--- utils/update-copyright.sh
+++ utils/update-copyright.sh
@@ -4,5 +4,5 @@ find . -type f -exec grep -Il "Copyright" {} \;     \
 | grep -v \.git \
 | while read file;                            \
 do \
-  LC_ALL=C sed -b -i "s/\(.*Copyright.*\)[0-9]\{4\}\( *Vitaly Novichkov\)/\1`date +%Y`\2/" "$file"; \
+  LC_ALL=C sed -b -i "s/\(.*Copyright.*\)[0-9]\{4\}\( *Vitali\?y Novichkov\)/\1`date +%Y`\2/" "$file"; \
 done
