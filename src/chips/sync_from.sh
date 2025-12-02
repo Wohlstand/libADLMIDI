cadcb3b2da3da9f324d1b8abb4f116d97a2f1ac6
Wohlstand <admin@wohlnet.ru>
2025-12-02 04:34:09 +0300

    Updated script to sync with OPL_ChipSet repo

--- src/chips/sync_from.sh
+++ src/chips/sync_from.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-SRC=`realpath $PWD/../../../OPL_ChipSet`
-DST=$PWD
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    RSYNC_ARGS=-vrt
+    SRC=$PWD/../../../OPL_ChipSet
+    DST=$PWD
+else
+    RSYNC_ARGS=-vArXtU
+    SRC=`realpath $PWD/../../../OPL_ChipSet`
+    DST=$PWD
+fi
 
 if [[ ! -d $SRC ]]; then
     echo "Can't sync! You should clone this repository https://github.com/Wohlstand/OPL_ChipSet and place it in the same directory as libADLMIDI's one. It should be named as \"OPL_ChipSet\"."
     exit 1
 fi
 
-rsync -vArXtU --exclude ".git" --exclude ".gitattributes" "$SRC/"* "$DST/"
+cd "$SRC"
+git pull
+
+cd "$DST"
+
+rsync $RSYNC_ARGS --exclude ".git" --exclude ".gitattributes" "$SRC/"* "$DST/"
