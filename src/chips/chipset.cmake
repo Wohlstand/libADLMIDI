c226aba58e5dc9bf4efdaa4c8d12189c648301c3
Wohlstand <admin@wohlnet.ru>
2026-06-06 14:49:24 +0300

    Removed duplicated line in the comment at chips.cmake [ci skip]

--- src/chips/chipset.cmake
+++ src/chips/chipset.cmake
@@ -2,7 +2,6 @@
 # Interfaces over Yamaha OPL3 (YMF262) chip emulators
 #
 # Copyright (c) 2017-2026 Vitaly Novichkov (Wohlstand)
-# Copyright (c) 2017-2026 Vitaly Novichkov (Wohlstand)
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
